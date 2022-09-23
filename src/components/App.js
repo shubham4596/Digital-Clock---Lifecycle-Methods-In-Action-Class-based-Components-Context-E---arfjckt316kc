e889070c4044b9923100bafc3daefb2a7eb0f4ce
shubham4596 <104636433+shubham4596@users.noreply.github.com>
2022-09-23 20:29:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
-
-  return (
-    <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
-}
-
-
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+const App = () => {  const [dateTime, setdateTime] = useState("");  
+                   const updateTime = () => {    let tempTime = new Date().toLocaleString();    
+                                             setdateTime(tempTime);  
+                                            };  
+                   useEffect(() => {    let tempTime = new Date().toLocaleString();    
+                                    setdateTime(tempTime);    
+                                    let intrval = setInterval(updateTime, 1000);    
+                                    return () => {      clearInterval(intrval);
+                                                 };  
+                                   }, []);
+                   
+  return (    <div id="main">      
+          <div className="date-time">{dateTime}</div>    
+</div>  
+);
+};
 export default App;
